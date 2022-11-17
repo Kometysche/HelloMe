58922554c3a0adf38472051deef280ed37c50cb2
Jana Dušková <jana.duskova14@gmail.com>
2022-11-17 15:26:00 +0100

    Changes

---
+++ Wednesday/src/Cuboid.java
@@ -0,0 +1,20 @@
+public class Cuboid {
+    public static void main(String[] args) {
+        // Write a program that stores 3 sides of a cuboid as variables (doubles)
+        // The program should write the surface area and volume of the cuboid.
+        //
+        // Set the 3 sides to 10.4, 13.5, 8.2 and your program should
+        // produce the following output:
+        // Surface Area: 672.76
+        // Volume: 1151.28
+        double a = 10.4;
+        double b = 13.5;
+        double c = 8.2;
+
+        double surface = ((2 * a * b) + (2 * b * c) + (2 * c * a));
+        double volume = (a * b * c);
+        System.out.println(surface);
+        System.out.println(volume);
+
+    }
+}
