71a0792d8fdf75a33fa7359a24120304271623cc
Jana Dušková <jana.duskova14@gmail.com>
2022-11-22 16:59:31 +0100

    WriteLines done.

--- FileIO/src/countLines.java
+++ FileIO/src/countLines.java
@@ -3,7 +3,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
-import java.lang.String;
+
 
 public class countLines {
     public static void main(String[] args) {
